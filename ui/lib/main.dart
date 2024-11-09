b3c96aab64ee9ea08c602a8dd48852f963fc59b1
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2024-11-09 23:08:54 +0100

    First version Fixes #1

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -39,6 +39,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class TestItem {
+  TestItem({required this.name});
+  String name;
+}
+
+var test = [
+  TestItem(name: 'Test1'),
+  TestItem(name: 'Test2'),
+];
+
 class StorageShortName extends StatefulWidget {
   const StorageShortName({super.key});
   @override
@@ -46,31 +56,74 @@ class StorageShortName extends StatefulWidget {
 }
 
 class _StorageShortName extends State<StorageShortName> {
-  var _Test = '';
+  var _test = '';
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(_Test),
-        Row(
+        Text(_test),
+        const Row(
+          children: [],
+        ),
+        DropdownButton<String>(
+          value: _test,
+          icon: const Icon(Icons.arrow_drop_down_circle),
+          items: const [
+            DropdownMenuItem<String>(
+              value: 'Test1',
+              child: Text('Test1'),
+            ),
+            DropdownMenuItem<String>(
+              value: 'Test2',
+              child: Text('Test2'),
+            ),
+          ],
+          onChanged: (value) => setState(() {
+            _test = value!;
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+class ScrewShortName extends StatefulWidget {
+  const ScrewShortName({super.key});
+  @override
+  State<ScrewShortName> createState() => _ScrewShortName();
+}
+
+class _ScrewShortName extends State<ScrewShortName> {
+  var _test = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(_test),
+        const Row(
           children: [],
         ),
         DropdownMenu<String>(
-          dropdownMenuEntries: [
+          dropdownMenuEntries: const [
             DropdownMenuEntry<String>(
               value: 'Test1',
               label: 'Test1',
+              trailingIcon: Icon(Icons.brightness_1),
             ),
             DropdownMenuEntry<String>(
               value: 'Test2',
               label: 'Test2',
+              trailingIcon: Icon(Icons.access_alarm_sharp),
             ),
           ],
           onSelected: (value) => setState(() {
-            _Test = value!;
+            _test = value!;
           }),
+          leadingIcon: const Icon(Icons.access_alarm_sharp),
         ),
       ],
     );
